cb1d5dd7a695ed07bb0980979ae4fc9d91b45bfb
franklin rodriguez <franklinserif@gmail.com>
2024-09-02 16:40:52 -0400

    feat(ServerBootstrap): :sparkles: add server class
    server starting point class with most necessary middlewares

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import cors from 'cors'
+
+class ServerBootstrap {
+    public app: express.Application = express()
+    private port: number = 8000
+
+    constructor() {
+        this.app.use(express.json())
+        this.app.use(express.urlencoded({ extended: true }))
+        this.app.use(morgan('dev'))
+        this.app.use(cors())
+        this.app.use(helmet())
+        this.listen()
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log('server running on port => ', this.port)
+        })
+    }
+}
+
+new ServerBootstrap()
