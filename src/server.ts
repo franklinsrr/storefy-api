f884d869e843d2de4ecf9960bdb7bf040469216d
franklin rodriguez <franklinserif@gmail.com>
2024-09-04 19:11:13 -0400

    feat(ServerBootstrap): initialize the database conection in the server

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 import { UserRouter } from '@routers/user.router'
 import { ConfigServer } from '@config/config'
+import { DataSource } from 'typeorm'
 
 export class ServerBootstrap extends ConfigServer {
     public app: express.Application = express()
@@ -14,6 +15,7 @@ export class ServerBootstrap extends ConfigServer {
 
         this.app.use(express.json())
         this.app.use(express.urlencoded({ extended: true }))
+        this.dbConnect()
         this.app.use(morgan('dev'))
         this.app.use(cors())
         this.app.use(helmet())
@@ -25,6 +27,10 @@ export class ServerBootstrap extends ConfigServer {
 
     routers(): Array<express.Router> {
         return [new UserRouter().router]
+    }
+
+    async dbConnect() {
+        await new DataSource(this.typeORMConfig).initialize()
     }
 
     public listen(): void {
