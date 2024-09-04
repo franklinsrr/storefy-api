5e897e70b6ccc6274f5b8e92bf60af46af2b12d2
franklin rodriguez <franklinserif@gmail.com>
2024-09-03 20:22:43 -0400

    test(test): :white_check_mark: user.routes.test.ts
    this will test if the route is working propely

---
+++ src/__tests__/server/routes/user.routes.test.ts
@@ -0,0 +1,30 @@
+import { describe, it, expect, vi } from 'vitest'
+import { Request, Response } from 'express'
+import { UserRouter } from '../../../routers/user.router'
+import { UserController } from '../../../controllers/user.controller'
+
+vi.mock('../src/controllers/user.controller')
+
+describe('UserRouter', () => {
+    it('debería manejar la ruta GET /users', () => {
+        const mockRequest = {} as Request
+        const mockResponse = {
+            status: vi.fn().mockReturnThis(),
+            json: vi.fn(),
+        } as unknown as Response
+
+        const userController = new UserController()
+        const userRouter = new UserRouter()
+
+        vi.spyOn(userController, 'getUser').mockImplementation((req, res) => {
+            res.status(200).json({ user: 'franklin rodriguez' })
+        })
+
+        userRouter['controller'].getUser(mockRequest, mockResponse)
+
+        expect(mockResponse.status).toHaveBeenCalledWith(200)
+        expect(mockResponse.json).toHaveBeenCalledWith({
+            user: 'franklin rodriguez',
+        })
+    })
+})
