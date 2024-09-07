00a33fabe1a0e9705fc3d82f3c19fe9663ff85d1
franklin rodriguez <franklinserif@gmail.com>
2024-09-07 08:27:30 -0400

    test(test): :white_check_mark: add test user without password

--- __tests__/server/entities/user.entity.test.ts
+++ __tests__/server/entities/user.entity.test.ts
@@ -44,4 +44,26 @@ describe('UserEntity', () => {
         expect(savedUser.phone).toBe('123456789')
         expect(savedUser.email).toBe('testuser@example.com')
     })
+
+    it('should create a UserEntity without password instance and assign properties correctly', async () => {
+        const userRepository = testDataSource.getRepository(UserEntity)
+
+        const user = new UserEntity()
+        user.username = 'testuser'
+        user.firstName = 'Test'
+        user.lastName = 'User'
+        user.userType = USER_TYPES[1]
+        user.phone = '123456789'
+        user.email = 'testuser@example.com'
+
+        const savedUser = await userRepository.save(user)
+
+        expect(savedUser.username).toBe('testuser')
+        expect(savedUser.firstName).toBe('Test')
+        expect(savedUser.lastName).toBe('User')
+        expect(savedUser.password).toBeNull()
+        expect(savedUser.userType).toBe(USER_TYPES[1])
+        expect(savedUser.phone).toBe('123456789')
+        expect(savedUser.email).toBe('testuser@example.com')
+    })
 })
