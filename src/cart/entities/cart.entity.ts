e207ab182a4870dbdbf426c8f2de1aeee315e5c4
franklin rodriguez <franklinserif@gmail.com>
2024-09-13 02:38:56 -0400

    feat(cart.entity.ts): add many to one relationship with costumer

--- src/cart/entities/cart.entity.ts
+++ src/cart/entities/cart.entity.ts
@@ -1,3 +1,9 @@
+import { Entity, ManyToOne } from 'typeorm'
+import { CostumerEntity } from '@costumer/entities/costumer.entity'
 import { BaseEntity } from '@shared/entities/base.entity'
 
 @Entity('carts')
+export class CartEntity extends BaseEntity {
+    @ManyToOne(() => CostumerEntity)
+    costumer!: CostumerEntity
+}
