0b76378474e7524c533f4856f3bed84eaad690cf
franklin rodriguez <franklinserif@gmail.com>
2024-09-13 04:18:29 -0400

    refactor(vitest.config.ts): remove and add paths

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,14 +5,18 @@ import path from 'path'
 export default defineConfig({
     resolve: {
         alias: {
-            '@controllers': path.resolve(__dirname, 'src/controllers'),
-            '@routers': path.resolve(__dirname, 'src/routers'),
             '@config': path.resolve(__dirname, 'src/config'),
             '@shared': path.resolve(__dirname, 'src/shared'),
-            '@entities': path.resolve(__dirname, 'src/entities'),
-            '@constants': path.resolve(__dirname, 'src/constants'),
             '@user': path.resolve(__dirname, 'src/user'),
-            '@interfaces': path.resolve(__dirname, 'src/interfaces'),
+            '@seller': path.resolve(__dirname, 'src/seller'),
+            '@costumer': path.resolve(__dirname, 'src/costumer'),
+            '@cart': path.resolve(__dirname, 'src/cart'),
+            '@cartItem': path.resolve(__dirname, 'src/cartItem'),
+            '@product': path.resolve(__dirname, 'src/product'),
+            '@productVariant': path.resolve(__dirname, 'src/productVariant'),
+            '@image': path.resolve(__dirname, 'src/image'),
+            '@sale': path.resolve(__dirname, 'src/sale'),
+            '@category': path.resolve(__dirname, 'src/category'),
         },
     },
     test: {
