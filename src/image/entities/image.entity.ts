217d93df1ef872bf0f38f0d1bd1e98192df4937d
franklin rodriguez <franklinserif@gmail.com>
2024-09-13 02:40:30 -0400

    feat(image.entity.ts): add relationship with product

--- src/image/entities/image.entity.ts
+++ src/image/entities/image.entity.ts
@@ -1,5 +1,6 @@
+import { ProductEntity } from '@product/entities/product.entity'
 import { BaseEntity } from '@shared/entities/base.entity'
-import { Column, Entity } from 'typeorm'
+import { Column, Entity, ManyToOne } from 'typeorm'
 
 @Entity('images')
 export class ImageEntity extends BaseEntity {
@@ -14,4 +15,7 @@ export class ImageEntity extends BaseEntity {
         name: 'alt_text',
     })
     altText!: string
+
+    @ManyToOne(() => ProductEntity, (product) => product.images)
+    product!: ProductEntity
 }
