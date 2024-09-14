772e2c69b1d30b4d328ff9b7c119df98a34976ef
franklin rodriguez <franklinserif@gmail.com>
2024-09-14 02:12:27 -0400

    feat(sale.entity.ts): add 2 relationship with seller and costumer

--- src/sale/entities/sale.entity.ts
+++ src/sale/entities/sale.entity.ts
@@ -1,6 +1,8 @@
 import { Column, Entity, ManyToOne } from 'typeorm'
 import { BaseEntity } from '@shared/entities/base.entity'
 import { ProductVariantEntity } from '@productVariant/entities/productVariant.entity'
+import { SellerEntity } from '@seller/entities/seller.entity'
+import { CostumerEntity } from '@costumer/entities/costumer.entity'
 
 @Entity('sales')
 export class SaleEntity extends BaseEntity {
@@ -26,4 +28,10 @@ export class SaleEntity extends BaseEntity {
 
     @ManyToOne(() => ProductVariantEntity)
     productVariant!: ProductVariantEntity
+
+    @ManyToOne(() => SellerEntity)
+    seller!: SellerEntity
+
+    @ManyToOne(() => CostumerEntity)
+    costumer!: CostumerEntity
 }
